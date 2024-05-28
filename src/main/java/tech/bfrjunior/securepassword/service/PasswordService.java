76d8f82c22e882d0b380396b5c2d3ecc0159812a
bfrjunior <beneditofernandor@gmail.com>
2024-05-28 19:12:35 -0300

    feat: Password service

---
+++ src/main/java/tech/bfrjunior/securepassword/service/PasswordService.java
@@ -0,0 +1,17 @@
+package tech.bfrjunior.securepassword.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PasswordService {
+
+    public List<String> validatePass(String pass) {
+
+        List<String> failures = new ArrayList<>();
+
+        return failures;
+    }
+}
